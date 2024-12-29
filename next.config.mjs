e73f4a494d7f457b707d0104f29d37e8c695c083
Fauzan Husain <achmadfauzan1340@gmail.com>
2024-12-29 19:07:55 +0800

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
+import nextPWA from 'next-pwa';
+
+const withPWA = nextPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development', // Nonaktifkan PWA di mode development
+});
+
 const nextConfig = {
   reactStrictMode: true,
+  ...withPWA,
 };
 
 export default nextConfig;
